76946fc7d32fa80b64799772bffa8bc175b97a64
DaniBalsera <daniseneca.df@gmail.com>
2024-11-09 10:40:25 +0100

    commit: Actualizacion repositorio

--- UD3/EjerciciosClase/guiadoSeneca/conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("LINE_CABECERA", 1);
-
-$caracteresBusqueda = array("Á", "á", "É", "é", "Í", "í", "Ó", "ó", "Ú", "ú", "Ú", "ü", "Ñ", "ñ", ",", "\"");
-
-$caracteresRemplaza = array("A", "a", "E", "e", "I", "i", "O", "o", "U", "u","N", "n", " ", " ");
-
-?>
